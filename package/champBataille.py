e1e0e5bcd880d1518106b1b49ef5c3c84fc465dc
Unknown <thomas.falcone06@gmail.com>
2017-11-15 18:18:42 +0100

    champBataille

--- package/champBataille.py
+++ package/champBataille.py
@@ -1,69 +1,80 @@
 # === === Champs Bataille === === #
-    
-def redresseCartes(Champ_Bataille)->Champ_Bataille
-    # Pre-condition : Aucune
-    # Post-condition : Aucune
-    # Resultat : Toutes les cartes du champ de bataille sont verticales. Le champ de bataille est donc modifié et renvoyé.
 
-def creerChampBataille(Joueur)-> Champ_Bataille
+def creerChampBataille():
     # Pre-condition : Le joueur entré en paramètre ne doit pas avoir de champ de bataille existant
     # Post-condition : Aucune
-    # Resultat : Toute les cartes du champ de bataille sont verticales 
+    # Resultat : Crée un champ de bataille vide. Crée un front et un arriere, les associent au champ créé et renvoie le champ.
+    
+    pass
+    
+def redresseCartes(champ):
+    # Pre-condition : le champ est de type ChampBataille
+    # Post-condition : Aucune
+    # Resultat : Toutes les cartes du champ de bataille sont verticales. Le champ de bataille est donc modifié et renvoyé. 
+    
+    pass
 
-def estVide(Champ_Bataille)->Bool
-    # Pre-condition : Aucune
+def estVide(champ) :
+    # Pre-condition : le champ est de type ChampBataille
     # Post-condition : Aucune
     # Resultat : Retourne True si aucune carte n'est placée sur le champs de bataille, false sinon
+    
+    pass
 
-def obtenirCarte(Champ_Bataille,Position)
-    # Pre-condition : La position entrée en paramètre doit correspondre à une position du plateau et une carte doit se trouver à cet emplacement. 
+def obtenirCarte(champ,pos)
+    # Pre-condition : La position (pos de type String) entrée en paramètre doit correspondre à une position du plateau et une carte doit se trouver à cet emplacement. champ est de type ChampBataille
     # Post-condition : Aucune
-    # Resultat : Indique la carte se trouvant à cette position
+    # Resultat : Renvoie la carte se trouvant à cette position SANS l'enlever
+    
+    pass
 
-def nbCarteCHampBataille(Champ_Bataille)->Int
+def nbCarteChampBataille(champ):
     # Pre-condition : Aucune
     # Post-condition : Aucune
-    # Resultat : Indique le nombre 
+    # Resultat : Indique le nombre de carte présente dans le champ de bataille
+    pass
 
-def front(Champ_Bataille)->Front
-    # Pre-condition : Le joueur doit être de la partie
+def front(champ):
+    # Pre-condition : le champ est de type ChampBataille
     # Post-condition : Aucune
-    # Resultat : retourne le champ de bataille du joueur entré en paramètre
+    # Resultat : retourne le front du champ entré en paramètre
+    
+    pass
 
-def arriere(Champ_Bataille)->Arriere
-    # Pre-condition : Le joueur doit être de la partie
+def arriere(champ):
+    # Pre-condition : le champ est de type ChampBataille
     # Post-condition : Aucune
-    # Resultat : retourne l'arriere du joueur entré en paramètre
+    # Resultat : retourne l'arriere du champ entré en paramètre
+    
+    pass
 
-def setArriere(Joueur,Arriere)->Arriere
+def touteHorizontale(champ):
     # Pre-condition : Aucune
     # Post-condition : Aucune
-    # Resultat : Assigne une nouvelle reserve au joueur donné en paramètre
+    # Resultat : Retourne True si toutes les cartes du champ de bataille sont horizontales, retourne False sinon
+    
+    pass
 
-def setFront(Joueur,Front)->Front
-    # Pre-condition : Aucune
-    # Post-condition : Aucune
-    # Resultat : Assigne une nouvelle reserve au joueur donné en paramètre
-
-def touteHorizontale(Champ_Bataille)->Bool
-    # Pre-condition : Aucune
-    # Post-condition : Aucune
-    # Resultat : Retourne True si toute les cartes du champs de bataille sont horizontal, retourne False sinon
-
-def estVidePosition(Champ_Bataille,String)->Bool
-    # Pre-condition : Aucune
+def estVidePosition(champ,pos):
+    # Pre-condition : pos est un string
     # Post-condition : Aucune
     # Resultat : Retourne True si la position contient une carte (en utilisant les fonction estVideFront et estVideArriere)
+    
+    pass 
 
-def verifArriere(Champ_Bataille,String)->Bool
+def verifArriere(champ,pos):
     # Pre-condition : Soit String (chaine de caractère) correspondant la position de la carte cible.
     # Post-condition : Aucune
     # Resultat : Retourne True si l'Arriere situé derrière la carte cible est occupé, retourne False sinon (ou si la carte cible donnée en paramètre n'est pas au front).
+    
+    pass
 
-def avancerCarte(Champ_Bataille,Str)->Champ_Bataille
-    # Pre-condition : La carte donnée en paramètres est au front et une autre carte se situe derriere elle
+def avancerCarte(champ,pos):
+    # Pre-condition : La position donnée en paramètres est occupée par une carte au front et une autre carte se situe derriere elle
     # Post-condition : Aucune
-    # Resultat : Renvoi un champs de bataille dans lequel la carte (située à l'arriere) est envoyée au front
+    # Resultat : Modifie le champ de bataille dans lequel la carte (située à l'arriere) est envoyée au front. La fonction retourne aussi ce champ. 
+    
+    pass
     
     
    
