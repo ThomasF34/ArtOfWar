3be9925a42f22b2f6451bfd95e468aabc149a33f
Unknown <thomas.falcone06@gmail.com>
2017-11-23 08:17:24 +0100

    Changement dossier

---
+++ FalconeGoncalves/RenduFalconeGoncalves/package/reserve.py
@@ -0,0 +1,31 @@
+#coding:utf-8
+# === === Reserve === === #
+
+def creerReserve() :
+    # Pre-condition : Aucune
+    # Post-condition : Aucune
+    # Resultat : Créer un element de type Reserve   
+    
+    pass 
+
+def premiereCarteReserve(reserve) :
+    # Pre-condition : reserve est de type Reserve
+    # Post-condition : Aucune
+    # Resultat : Retourne la premiere carte de la reserve du joueur
+    
+    pass 
+
+def nbCarteReserve(reserve) : 
+    # Pre-condition : la reserve est de type Reserve
+    # Post-condition : Aucune
+    # Resultat : Indique le nombre de carte présente dans la réserve
+    
+    pass
+
+def envoiReserve(reserve,carte): 
+    # Pre-condition : la reserve est de type Reserve et la carte de type Carte
+    # Post-condition : Aucune
+    # Resultat : la carte donnée est envoyée à la reserve !! EN FIN DE FILE !!
+    
+    pass
+    
